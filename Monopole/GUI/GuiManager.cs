dbd28b87b1703da488a5e450fbcbf207e4392ce2
Caleb Simpson <the.foner@gmail.com>
2012-12-08 12:41:21 -0500

    Change GuiManager to use a dictionary instead of a list.

--- Monopole/GUI/GuiManager.cs
+++ Monopole/GUI/GuiManager.cs
@@ -7,13 +7,21 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Atomic
 {
-    public class GuiManager : List<InputField>
+    public class GuiManager : Dictionary<string, InputField>
     {
         public InputField focus { get; private set; }
 
+		public void Add(InputField field)
+		{
+			string _key = "0";
+			while (ContainsKey(_key))
+				MathExtra.random.Next().ToString();
+			Add(_key, field);
+		}
+
         public void Update()
         {
-            foreach (InputField field in this)
+            foreach (InputField field in this.Values)
             {
                 if (field.enabled && Input.MLBPressed() && Input.mouse.X >= field.position.X && Input.mouse.Y >= field.position.Y && Input.mouse.X <= (field.position + field.size).X && Input.mouse.Y <= (field.position + field.size).Y)
                 {
@@ -25,7 +33,7 @@ namespace Atomic
                 field.Update();
             }
 
-            if (Input.KeyPressed(Keys.Tab) && focus != null && focus.tab != null)
+            if (Input.KeyPressed(Microsoft.Xna.Framework.Input.Keys.Tab) && focus != null && focus.tab != null)
             {
                 focus.focused = false;
                 focus.tab.focused = true;
@@ -35,7 +43,7 @@ namespace Atomic
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            foreach (InputField field in this)
+            foreach (InputField field in this.Values)
                 field.Draw(spriteBatch);
         }
     }
